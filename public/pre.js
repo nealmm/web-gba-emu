81bf5aaf2d38e12cc82bd6cffc7ed5ea48d6fe4f
Matt Neal <neal.matt.m@gmail.com>
2023-11-29 21:35:37 -0500

    Functional emulation

--- public/pre.js
+++ public/pre.js
@@ -1,5 +1,28 @@
 var Module = {
+    noInitialRun: true,
+
     canvas: function() {
         return document.getElementById('canvas');
-    }()
+    }(),
+
+    preRun: [function() {
+        const dialog = document.getElementById('rom-dialog');
+        const input = document.getElementById('rom-file-input');
+        const button = document.getElementById('play-rom-button');
+
+        dialog.showModal();
+
+        button.addEventListener('click', async () => {
+            const file = input.files[0];
+            const buffer = new Uint8Array(await file.arrayBuffer());
+            const stream = FS.open(file.name, 'w+');
+
+            FS.write(stream, buffer, 0, buffer.length);
+            FS.close(stream);
+
+            dialog.close();
+
+            Module.callMain([file.name]);
+        });
+    }]
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,119 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+#include <SDL.h>
+
+#include <nba/device/input_device.hpp>
+#include <nba/device/video_device.hpp>
+
+#include <platform/config.hpp>
+#include <platform/device/sdl_audio_device.hpp>
+#include <platform/loader/bios.hpp>
+#include <platform/loader/rom.hpp>
+#include <platform/emulator_thread.hpp>
+
+#include <emscripten.h>
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
+u32 *framebuffer;
+
+auto input_dev = std::make_shared<nba::BasicInputDevice>();
+
+struct Screen : nba::VideoDevice {
+    void Draw(u32 *buffer) {
+        framebuffer = buffer;
+    }
+};
+
+void frame() {
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_LockSurface(surface);
+    }
+
+    u32 *pixels = (u32*)surface->pixels;
+
+    for (int i = 0; i < 38400; i++) {
+        pixels[i] = framebuffer[i];
+    }
+
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_UnlockSurface(surface);
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    SDL_DestroyTexture(texture);
+}
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(240, 160, 0, &window, &renderer);
+
+    surface = SDL_CreateRGBSurface(0, 240, 160, 32, 0, 0, 0, 0);
+
+    auto config = std::make_shared<nba::PlatformConfig>();
+
+    config->video_dev = std::make_shared<Screen>();
+    config->audio_dev = std::make_shared<nba::SDL2_AudioDevice>();
+    config->input_dev = input_dev;
+
+    auto core = nba::CreateCore(config);
+
+    auto rom_path = fs::path{argv[1]};
+    auto save_path = fs::path{argv[1]}.replace_extension(".sav");
+
+    auto save_type = config->cartridge.backup_type;
+
+    auto force_gpio = nba::GPIODeviceType::None;
+
+    if (config->cartridge.force_rtc) {
+        force_gpio = force_gpio | nba::GPIODeviceType::RTC;
+    }
+
+    if (config->cartridge.force_solar_sensor) {
+        force_gpio = force_gpio | nba::GPIODeviceType::SolarSensor;
+    }
+
+    switch (nba::BIOSLoader::Load(core, "bios.bin")) {
+        case nba::BIOSLoader::Result::CannotFindFile:
+            std::cerr << "Cannot find BIOS file" << std::endl;
+            return -1;
+
+        case nba::BIOSLoader::Result::CannotOpenFile:
+            std::cerr << "Cannot open BIOS file" << std::endl;
+            return -1;
+
+        case nba::BIOSLoader::Result::BadImage:
+            std::cerr << "Bad BIOS image" << std::endl;
+            return -1;
+
+        case nba::BIOSLoader::Result::Success:
+            break;
+    }
+
+    switch (nba::ROMLoader::Load(core, rom_path, save_path, save_type, force_gpio)) {
+        case nba::ROMLoader::Result::CannotFindFile:
+            std::cerr << "Cannot find ROM file" << std::endl;
+            return -1;
+
+        case nba::ROMLoader::Result::CannotOpenFile:
+            std::cerr << "Cannot open ROM file" << std::endl;
+            return -1;
+
+        case nba::ROMLoader::Result::BadImage:
+            std::cerr << "Bad ROM image" << std::endl;
+            return -1;
+
+        case nba::ROMLoader::Result::Success:
+            break;
+    }
+
+    auto emu_thread = std::make_unique<nba::EmulatorThread>(core);
+    emu_thread->Start();
+
+    emscripten_set_main_loop(frame, 0, 1);
 }
